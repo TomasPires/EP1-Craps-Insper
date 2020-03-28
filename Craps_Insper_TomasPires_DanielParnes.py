7336643161225489b7b5492b2f6632772a2f112d
Tomas Pires <tomassp@al.insper.edu.br>
2020-03-28 18:17:57 -0300

    Arquivo do trabalho

---
+++ Craps_Insper_TomasPires_DanielParnes.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 28 18:13:40 2020
+
+@author: tprfe
+"""
+
+#EP1-Craps Insper
+#Produzido por Tomás Selaibe Pires e Daniel Parnes
+#Design de Software - 2020.1
